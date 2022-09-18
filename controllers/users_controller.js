b26883acc0a47671d2e553e6ce37ecb76dee1399
Rohan Shrivastava <rohanshrivastava10000@gmail.com>
2022-09-18 18:24:33 +0530

    Created Manual authentication using Express and MongoDB

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,20 @@
 const User = require("../models/user");
 
 module.exports.profile = function (request, response) {
-  response.render("profile", {
-    title: "Codeial | Profile",
-  });
+  if (request.cookies.user_id) {
+    User.findById(request.cookies.user_id, function (error, user) {
+      if (user) {
+        return response.render("user_profile", {
+          title: "User Profile",
+          user: user,
+        });
+      } else {
+        return response.redirect("/users/sign-in");
+      }
+    });
+  } else {
+    return response.redirect("/users/sign-in");
+  }
 };
 
 // render the sign up page
@@ -22,10 +33,12 @@ module.exports.signin = function (request, response) {
 
 // get the sign up data.
 module.exports.create = function (request, response) {
+  // Incorrect password
   if (request.body.password != request.body.confirm_password) {
     return response.redirect("back");
   }
 
+  // Find user
   User.findOne({ email: request.body.email }, function (error, user) {
     if (error) {
       console.log("error in finding user in signing up");
@@ -47,5 +60,35 @@ module.exports.create = function (request, response) {
 
 // sign in and create a session for the user.
 module.exports.createSession = function (request, response) {
-  // TODO Later
+  // find the user
+  User.findOne({ email: request.body.email }, function (error, user) {
+    if (error) {
+      console.log("error in finding user in signing up");
+      return;
+    }
+    // handle user found
+    if (user) {
+      // handle password which doesn't match.
+      if (user.password != request.body.password) {
+        return response.redirect("back");
+      }
+      // handle session creation
+      response.cookie("user_id", user.id);
+      return response.redirect("/users/profile");
+    } else {
+      // handle user not found
+      return response.redirect("back");
+    }
+  });
 };
+
+module.exports.signout = function (request,response) {
+  // clearing the cookie for sign out
+  if(request.cookies.user_id){
+    response.clearCookie("user_id");
+    return response.redirect("/users/sign-in");
+  }
+  else{
+    return response.redirect('back');
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,7 @@ router.get('/profile',usersController.profile);
 router.get('/sign-up',usersController.signup);
 router.get('/sign-in',usersController.signin);
 router.post('/create',usersController.create);
+router.post('/create-session',usersController.createSession);
+router.post('/sign-out',usersController.signout);
 
 module.exports = router;
